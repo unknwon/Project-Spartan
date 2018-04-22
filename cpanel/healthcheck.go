b74b657daf0d7a7c23e56cc5846f2998def48092
Unknwon <u@gogs.io>
2018-04-21 23:51:21 -0400

    cpanel: allow failed DB connection at start

--- cpanel/healthcheck.go
+++ cpanel/healthcheck.go
@@ -22,6 +22,12 @@ import (
 var haproxyRegistry, serverRegistry, databaseRegistry *registry.Registry
 var dbCoons = map[string]*gorm.DB{}
 
+// newDBConn tries to connect to a database with given information and returns the connection.
+func newDBConn(in *registry.Instance) (*gorm.DB, error) {
+	sec := setting.Config.Section("database." + in.Name)
+	return gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/rportal", sec.Key("USER"), sec.Key("PASSWORD"), in.Address))
+}
+
 func init() {
 	healthCheckClient = &http.Client{
 		Timeout: setting.HealthCheck.Timeout,
@@ -32,10 +38,11 @@ func init() {
 
 	// Setup database connections
 	for _, in := range databaseRegistry.Instances {
-		sec := setting.Config.Section("database." + in.Name)
-		x, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/rportal", sec.Key("USER"), sec.Key("PASSWORD"), in.Address))
+		x, err := newDBConn(in)
 		if err != nil {
-			log.Fatal(0, "Fail to open database connection: %v", err)
+			log.Warn("Failed to open database connection for '%s': %v", in.Name, err)
+			in.Status = registry.STATUS_DOWN
+			continue
 		}
 		dbCoons[in.Name] = x
 	}
@@ -50,14 +57,14 @@ func sendHealthCheckRequest(in *registry.Instance) bool {
 	resp, err := healthCheckClient.Get("http://" + in.Address + "/healthcheck")
 	if err != nil {
 		if _, ok := err.(net.Error); !ok {
-			log.Error(2, "Fail to perform health check for '%s': %v", in, err)
+			log.Error(2, "Failed to perform health check for '%s': %v", in, err)
 		}
 		return false
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		log.Error(2, "Fail to perform health check for '%s': status code is %d not 200", in, resp.StatusCode)
+		log.Error(2, "Failed to perform health check for '%s': status code is %d not 200", in, resp.StatusCode)
 		return false
 	}
 	return true
@@ -81,8 +88,21 @@ func HealthCheck() {
 
 	// FIXME: Need to reconnect if the database was down last time, otherwise Ping will always fail.
 	for _, in := range databaseRegistry.Instances {
+		// Retry connect if previously failed
+		if dbCoons[in.Name] == nil {
+			x, err := newDBConn(in)
+			if err != nil {
+				log.Warn("Failed to open database connection for '%s': %v", in.Name, err)
+				continue
+			}
+			dbCoons[in.Name] = x
+		}
+
+		// Note: For some reasons, GORM Ping cannot detect database down without a failed query.
+		dbCoons[in.Name].HasTable("resellers")
 		if err := dbCoons[in.Name].DB().Ping(); err != nil {
-			log.Error(2, "Fail to perform health check for '%s': %v", in, err)
+			log.Error(2, "Failed to perform health check for '%s': %v", in, err)
+			dbCoons[in.Name] = nil
 			in.Status = registry.STATUS_DOWN
 		} else {
 			in.Status = registry.STATUS_RUNNING
